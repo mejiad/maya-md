a0918c9180e2d2c9698bd920393505a229119b4a
mejiad <mejiad@evol-tech.com>
2022-06-30 23:37:25 -0500

    plot de cluster

--- AlbuminShiny/app.R
+++ AlbuminShiny/app.R
@@ -13,6 +13,7 @@ library(readxl)
 library(ggplot2)
 library(tidyverse)
 library(plotly)
+library(DT)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -23,6 +24,12 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
+            sliderInput("clusters.Input",
+                        "Number of Clusters:",
+                        min = 0,
+                        max = 12,
+                        step = 1,
+                        value = 3),
             sliderInput("albumin.Input",
                         "Albumin:",
                         min = 0,
@@ -52,14 +59,10 @@ ui <- fluidPage(
         # Show a plot of the generated distribution
         mainPanel(
           tabsetPanel(
-            tabPanel("Albumin - Vitamin D", plotOutput("plot1")),
-            tabPanel("Albumin -Vitamin B12", plotOutput("plot3")),
-            tabPanel("Two", plotlyOutput("plot2")),
-            tabPanel("Four", plotOutput("plot4")),
-            tabPanel("Five", plotOutput("plot5")),
-            tabPanel("Six", plotOutput("plot6")),
-            tabPanel("Seven", plotOutput("plot7")),
-            tabPanel("Eight", plotOutput("plot8"))
+            tabPanel("Data Table", DTOutput("datatable")),
+            tabPanel("Albumin - Vitamin D", plotOutput("vitD")),
+            tabPanel("Albumin -Vitamin B12", plotOutput("vitB12")),
+            tabPanel("Cluster Analysis", plotlyOutput("cluster"))
           )
         )
     )
@@ -68,9 +71,6 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   set.seed(417)
-  temp <- rnorm(100, mean=30, sd=5)
-  pressure <- rnorm(100)
-  dtime <- 1:100
   
   data <- read_excel("MayaMD_1587.xlsx")
   df <- data.frame(data)
@@ -95,86 +95,66 @@ server <- function(input, output) {
   df_mod$Vit.D.assay <- as.numeric(df_mod$Vit.D.assay)
   df_mod$Vitamin.B12..Serum <- as.numeric(df_mod$Vitamin.B12..Serum)
   
-  CENTERS = 3
+  CENTERS <- 4
+  
   df_albumin <- df_mod %>% filter(!is.na(Albumin...Serum) & !is.na(Vit.D.assay) & !is.na(Vitamin.B12..Serum)) 
   albumin_kmean <- df_mod %>% filter(!is.na(Albumin...Serum) & !is.na(Vit.D.assay) & !is.na(Vitamin.B12..Serum)) %>% select(Albumin...Serum, Vit.D.assay, Vitamin.B12..Serum)
+  df_bmi <- df_mod %>% filter(!is.na(Albumin...Serum) & !is.na(Vit.D.assay) & !is.na(Vitamin.B12..Serum))  %>% select(Albumin...Serum, Vit.D.assay, Vitamin.B12..Serum, BMI.Group)
   
-  # Fitting K-Means clustering Model to training dataset
-  
-  set.seed(240) # Setting seed
-  kmeans.re <- kmeans(albumin_kmean, centers = CENTERS, nstart = 10)
-  kmeans.re
   
   header_str <- paste("MK-means ", CENTERS, " cluster", sep=" ")
   
-  # plot(albumin_kmean[c("Albumin...Serum", "Vit.D.assay")],
-  # col = kmeans.re$cluster)
-  # plot(albumin_kmean[c("Albumin...Serum", "Vit.D.assay")],
-  # col = kmeans.re$cluster,
-  # main = header_str )
-  
-  # kmeans.re$centers
-  # kmeans.re$centers[, c("Albumin...Serum", "Vit.D.assay")]
-  
-    dat <- data.frame(xvar = 1:20 + rnorm(20, sd=3),
-                      yvar = 1:20 + rnorm(20, sd=3))
-    dat2 <- data.frame(xvar = 1:100 + rnorm(100, sd=3),
-                      yvar = 1:200 + rnorm(200, sd=3))
-    dat3 <- data.frame(xvar = 1:10 + rnorm(10, sd=3),
-                      yvar = 1:10 + rnorm(10, sd=3))
-    
-    output$plot1 <- renderPlot({
-        bmiMin <- as.numeric(input$bmiInput[1])
-        bmiMax <- as.numeric(input$bmiInput[2])
-        vitD <- as.numeric(input$vitD.Input)
-        albuminInput <- as.numeric(input$albumin.Input)
-    
-        df_bmi <- df_albumin %>% filter(BMI.Group >= bmiMin & BMI.Group <= bmiMax & Vit.D.assay < vitD & Albumin...Serum < albuminInput)  
-      
+    output$vitD <- renderPlot({
+        df_bmi <- df_albumin %>% filter(BMI.Group >= input$bmiInput[1] & 
+                                          BMI.Group <= input$bmiInput[2] & 
+                                          Vit.D.assay < input$vitD.Input & 
+                                          Vitamin.B12..Serum < input$vitB12.Input &
+                                          Albumin...Serum < input$albumin.Input)  
+        
         fig <- df_bmi %>% ggplot(aes(x=Albumin...Serum, y = Vit.D.assay)) + geom_point(aes(col=factor(BMI.Group))) + xlim(0,6) + ylim(0,100) +
           geom_hline(yintercept = 3.0, linetype = 'dotted', col = 'red')  + annotate("text", x = 0, y = 3.0, label = "Vit.D 3", vjust=-0.5) +
           geom_vline(xintercept = 3.5, linetype = 'dotted', col = 'blue') + annotate("text", x = 3.5, y = 100.0, label = "Albumin 3.5", hjust=0.5) 
         fig
     })
     
-    output$plot2 <- renderPlotly({
-      plot_ly(x=temp, y=pressure, z=dtime, type="scatter3d", mode="markers", color=temp)
+    output$cluster <- renderPlotly({
+      numClusters <- as.numeric(input$clusters.Input)
+      
+      set.seed(240) # Setting seed
+      albumin_kmean <- albumin_kmean %>% filter(Albumin...Serum < input$albumin.Input & 
+                                                  Vit.D.assay < input$vitD.Input & 
+                                                  Vitamin.B12..Serum < input$vitB12.Input)
+      
+      kmeans.re <- kmeans(albumin_kmean, centers = numClusters, nstart = 3)
+      kmeans.re
+      albumin_axis <- albumin_kmean$Albumin...Serum
+      vitaminD_axis <- albumin_kmean$Vit.D.assay
+      vitaminB12_axis <- albumin_kmean$Vitamin.B12..Serum
+  
+      color <- kmeans.re$cluster
+  
+      cluster_plot <- plot_ly(x=albumin_axis, y=vitaminB12_axis, z=vitaminD_axis, type="scatter3d", mode="markers", color=factor(color), size = 2 )
+      cluster_plot <- cluster_plot %>% layout(autosize = F, width = 800, height = 800, scene = list(xaxis = list(title="Albumin"), yaxis=list(title="Vitamin B12"), zaxis=list(title="Vitamin D")) )
+      cluster_plot
     })
     
-    output$plot3 <- renderPlot({
-        bmiMin <- as.numeric(input$bmiInput[1])
-        bmiMax <- as.numeric(input$bmiInput[2])
-        vitD <- as.numeric(input$vitD.Input)
-        albuminInput <- as.numeric(input$albumin.Input)
-        vitB12.Input <- as.numeric(input$vitB12.Input)
-    
-        df_vitB12 <- df_albumin %>% filter(BMI.Group >= bmiMin & BMI.Group <= bmiMax & Vitamin.B12..Serum < vitB12.Input & Albumin...Serum < albuminInput)  
-      
-        figVitB12 <- df_vitB12 %>% ggplot(aes(x=Albumin...Serum, y = Vitamin.B12..Serum)) + geom_point(aes(col=factor(BMI.Group))) + xlim(0,6) + 
+    output$vitB12 <- renderPlot({
+        df_bmi <- df_albumin %>% filter(BMI.Group >= input$bmiInput[1] & 
+                                          BMI.Group <= input$bmiInput[2] & 
+                                          Vit.D.assay < input$vitD.Input & 
+                                          Vitamin.B12..Serum < input$vitB12.Input &
+                                          Albumin...Serum < input$albumin.Input)  
+        
+        figVitB12 <- df_bmi %>% ggplot(aes(x=Albumin...Serum, y = Vitamin.B12..Serum)) + geom_point(aes(col=factor(BMI.Group))) + xlim(0,6) + 
           geom_hline(yintercept = 50.0, linetype = 'dotted', col = 'red')  + annotate("text", x = 0, y = 50.0, label = "B12 50", vjust=-0.5) +
           geom_vline(xintercept = 3.5, linetype = 'dotted', col = 'blue') + annotate("text", x = 3.5, y = 1000.0, label = "Albumin 3.5", hjust=0.5) 
         figVitB12
     })
-    output$plot4 <- renderPlot({
-        fig <- dat2 %>% ggplot(aes(x=xvar, y = yvar)) + geom_point()
-        fig
+    
+    output$datatable <- ({
+      renderDT(df_bmi, options = list(lenghtChange = FALSE))
     })
-    output$plot5 <- renderPlot({
-        fig <- dat2 %>% ggplot(aes(x=xvar, y = yvar)) + geom_point()
-        fig
-    })
-    output$plot6 <- renderPlot({
-        fig <- dat2 %>% ggplot(aes(x=xvar, y = yvar)) + geom_point()
-        fig
-    })
-    output$plot7 <- renderPlot({
-        fig <- dat2 %>% ggplot(aes(x=xvar, y = yvar)) + geom_point()
-        fig
-    })
-    output$plot8 <- renderPlot({
-        fig <- dat2 %>% ggplot(aes(x=xvar, y = yvar)) + geom_point()
-        fig
-    })
+    
 }
 
 # Run the application 
